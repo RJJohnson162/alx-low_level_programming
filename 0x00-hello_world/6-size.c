6054eea88b7cccfc914b3ae8b909b50a669103e6
RJJohnson162 <richardnyamwamu@gmail.com>
2022-06-30 17:00:10 -0700

    C Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main- prints the size of the various types
+ * main - prints the size of the various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
