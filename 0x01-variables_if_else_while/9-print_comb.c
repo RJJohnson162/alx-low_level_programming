73ffabd5fe77317f4bfabcc0fcbaa296e66b11e9
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-04 17:15:54 +0300

    minor changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 int ch;
-for(ch = 48; ch <= 57; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
-if(ch !=57 )
+if (ch != 57 )
 {
 putchar(44);
 putchar(32);
 }
 }
-putchar(10);
+/*putchar(10);*/
 
 return (0);
 }
