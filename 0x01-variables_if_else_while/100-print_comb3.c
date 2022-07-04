d12c8ef9cc96ee1f89521d918e56f385522cc62d
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-04 23:08:53 +0300

    extra

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it's executed
+ * and print the last digit of the number stored in the variables n
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch ++)
+{
+for (n = 49; n <= 57; n++)
+{
+if (n > ch)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
