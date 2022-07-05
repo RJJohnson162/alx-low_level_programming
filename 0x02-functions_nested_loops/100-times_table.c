97519d7c75f1e14d6a74e6cf44d03f48a5ae424c
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-06 02:09:14 +0300

    time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,8 +14,8 @@ void print_times_table(int n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			z = a * b;
-			if (z > 99)
+			c = a * b;
+			if (c > 99)
 			{
 				_putchar(c / 100 + '0');
 				_putchar((c / 10 % 10) + '0');
