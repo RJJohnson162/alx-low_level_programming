339cb3e6ca6dcb085bcfcb6104880908e530d05e
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-05 17:09:52 +0300

    printing alphabets

---
+++ 1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> Print out the alphabet in lower case 
+ */
+
+void print_alphabet(void)
+{
+    int a;
+    for (a = 'a'; a <= 'z'; a++)
+    {
+        _putchar(a);
+    }
+    _putchar("\n");
+}
