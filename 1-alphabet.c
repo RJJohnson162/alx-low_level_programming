eb96258ecebef1dc430bd54023af19ebde2be2a8
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-05 18:31:56 +0300

    Printing the alphabet

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * print_alphabet -> Print out the alphabet in lower case 
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-int a;
-for (a = 'a'; a <= 'z'; a++)
+int j;
+for (j = 'a'; j <= 'z'; j++)
 {
-_putchar(a);
-}
+_putchar(j);
+} 
 _putchar("\n");
 }
