435e7a751fb725811485f8bd0b71e4853c308092
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-06 01:03:44 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,17 @@ int print_last_digit(int n)
 int x;
 
 if (n < 0)
-n = -n;
-x = n % 10;
-_putchar(x + '0');
-return (x);
+{
+    n = -n;
+    x = n % 10;
+    _putchar(x + '0');
+    return (x);
+}
+else
+{
+    x = n % 10;
+    _putchar(x + '0');
+    return (x);
+}
+
 }
