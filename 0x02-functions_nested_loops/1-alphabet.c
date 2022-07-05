600f8f6cc621b96c504a25c2a5714bc487a41f18
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-05 22:52:47 +0300

    printing the lower case alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-    int j;
-    for (j = 'a'; j <= 'z'; j++)
-    {
-        _putchar(j);
-    }
-    _putchar(10);
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar(10);
 }
