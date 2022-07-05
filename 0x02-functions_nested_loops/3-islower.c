52ec49bb86a2da4af34043959533d2b1cbe9a8ac
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-05 23:36:03 +0300

    checking if char is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * -islower -> checks if the alphabet is lower case
+ * _islower -> checks if the alphabet is lower case
  * Return: returns 1 and 0 depending on the condition
  */
 
