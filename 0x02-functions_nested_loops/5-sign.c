202ff4465d82e939d8bfb5cb0baa4e0c814bed7a
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-06 00:09:10 +0300

    printing sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign -> checks if the alphabet is lower case
+ * @n: a character argument
+ * Return: returns 1 and 0 depending on the condition
+ */
+
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (0);
+    }
+    
+    
+}
