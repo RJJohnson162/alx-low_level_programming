11c85e96ede903cb878528f2281989f75294f9ed
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-05 23:40:18 +0300

    char if lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 
 /**
  * _islower -> checks if the alphabet is lower case
+ * o -> parameter in the function
+ * r -> parameter in the function
+ * c -> parameter in the function
  * Return: returns 1 and 0 depending on the condition
  */
 
 int _islower(int c)
 {
-
 return (c >= 'a' && c <= 'z');
 }
