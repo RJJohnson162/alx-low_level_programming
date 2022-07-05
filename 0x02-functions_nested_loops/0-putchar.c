f7eff9bc8769ab1e593187deadac404a28f232b6
RJJohnson162 <richardnyamwamu@gmail.com>
2022-07-05 01:56:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints _putchar when executed
+ * Return: always give 0
+ */
+
+int main(void)
+{
+printf("_putchar");
+putchar(10);
+return (0);
+}
