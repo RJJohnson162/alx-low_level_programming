73759ed2623f79a7eaea3b0e1333c81da27fe653
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-07 23:30:56 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle when executed
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int r, h, s;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (s = size - r; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (h = 1; h <= r; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
