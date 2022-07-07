2517cac1ccf34346cd3939280c11b1341b3d8146
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-07 22:27:42 +0300

    print 14x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,14 @@ void more_numbers(void)
 {
 
 int a, b;
-	for (a = 1; a <= 10; a++)
-	{
-		for (b = 0; b <= 14; b++)
-		{
-			if (b >= 10)
-			_putchar('1');
-			_putchar(b % 10 + '0');
-		}
-			_putchar('\n');
-		}
+for (a = 1; a <= 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b >= 10)
+_putchar('1');
+_putchar(b % 10 + '0');
+}
+_putchar('\n');
+}
 }
