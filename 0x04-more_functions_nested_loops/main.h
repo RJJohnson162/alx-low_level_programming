507669300074a8bc8cf37be5a38a0f3321bc2e27
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-08 16:36:04 +0300

    correction

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int print_number(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
--- 0x04-more_functions_nested_loops/print_number.c
+++ 0x04-more_functions_nested_loops/print_number.c
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int print_number(char c)
+int print_number(int n)
 {
 	return (write(1, &c, 1));
 }
