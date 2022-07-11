ac4d6b1a7b31341078bb4837a2a61cd145ac2bf2
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-12 00:19:20 +0300

    Printing to stdout

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - printing to stdout
+ * @str: string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
