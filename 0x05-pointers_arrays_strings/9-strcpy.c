78b563a869a69770ac125c8a45516dc76423c687
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-12 00:40:00 +0300

    Destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointed to
+ * @src: source
+ * @dest: destination
+ * Return: return the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
