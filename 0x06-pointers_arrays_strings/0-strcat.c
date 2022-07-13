6fc85c097e3b6f89f2d1fa95ce279abf5fd333de
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-13 17:18:11 +0300

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
     a = b = 0;
     while (*(dest + a))
         a++;
-    while ((*(dest + a) = *(srs + b)))
+    while ((*(dest + a) = *(src + b)))
     {
         a++;
         b++;
