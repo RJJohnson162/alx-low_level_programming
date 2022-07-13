7efd3e75642d9075c0dfa8f069331184fe8ea521
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-13 16:39:45 +0300

    Concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings: *dest & *src
+ * @src: src string
+ * @dest: dest string
+ * Return: give the address of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int a, b;
+
+    a = b = 0;
+    while (*(dest + a))
+        a++;
+    while ((*(dest + a) = *(srs + b)))
+    {
+        a++;
+        b++;
+    }
+    return (dest);
+}
