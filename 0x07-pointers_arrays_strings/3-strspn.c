a04a721fa55c72f95a58e62837578219dad05a1a
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-18 20:52:53 +0300

    length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int a, b;
+unsigned int a, b;
 
-    for (a = 0; s[a]; a++)
-    {
-        for (b = 0; accept[b]; b++)
-        {
-            if (s[a] == accept[b])
-            break;
-        }
-        if (!accept[b])
-        break;
-    }
-    return (a);    
+for (a = 0; s[a]; a++)
+{
+for (b = 0; accept[b]; b++)
+{
+if (s[a] == accept[b])
+break;
+}
+if (!accept[b])
+break;
+}
+return (a);    
 }
