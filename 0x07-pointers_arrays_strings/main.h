af574d1748e1a9ac514019aa7ceddaae1669d2d3
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-18 17:50:59 +0300

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
+
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -11,6 +14,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-
-#endif // !MAIN_H
+#endif/*MAIN_H*/
