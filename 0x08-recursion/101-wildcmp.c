22d82a72d31203b13ede15e6d0766b5fe3e1fa20
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2022-07-21 00:00:28 +0300

    WildCmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,33 @@
 #include "main.h"
 
 /**
- * test - tests if wildcard is a proper match
- * @s5: string to compare
- * @s6: wildcard match to compare
- * @t5: first spot that was checked
- * @t6: first spot in wildcard comparison
- *
- * Return: spot that matches or NULL
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
  */
-
-char *test(char *s5, char *s6, char *t5, char *t6)
+int str_checker(char *s1, char *s2, int i, int j)
 {
-	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
-		return (t5);
-	else if (*s5 != *s6)
-		return (wildcard(s5, t6));
-	else
-		return (test(s5 + 1, s6 + 1, t5, t6));
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
 /**
- * wildcard - checks for the wildcards
- * @s3: string to compare to
- * @s4: string to check
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * Return: pointer to spot in s3 that matches s4
+ * Return: 1 if are considered identical.
  */
-
-char *wildcard(char *s3, char *s4)
-{
-	if (*s3 != *s4)
-	{
-		if (*s3 == '\0')
-			return (0);
-		return (wildcard(s3 + 1, s4));
-	}
-	return (test(s3 + 1, s4 + 1, s3, s4));
-}
-
-/**
- * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string, may contain wildcard *
- *
- * Return: 1 if match, 0 if not
- */
-
 int wildcmp(char *s1, char *s2)
 {
-	char *p;
-
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
-		p = wildcard(s1, s2 + 1);
-		if (p == 0)
-			return (0);
-		return (wildcmp(p + 1, s2 + 2));
-	}
-	else if (*s1 != *s2)
-		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	return (wildcmp(s1 + 1, s2 + 1));
+	return (str_checker(s1, s2, 0, 0));
 }
